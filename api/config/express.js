4bbf25373435d4d27cd43f147e8ce5095febff21
AndonGeorgiev <andon2110@gmail.com>
2022-03-22 18:47:05 +0200

    add routes file

--- api/config/express.js
+++ api/config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const routes = require('./../routes.js');
 
 function expressConfig(app) {
     app.use(express.json());
     app.use(cookieParser);
+    app.use(routes);
 
 }
 
