6be97f432eaa087f3f602531625a9d5fe45a88c4
AndonGeorgiev <andon2110@gmail.com>
2022-04-11 14:54:17 +0300

    add 404 page and routing paths

--- app/real-estate-agency/src/app/app.module.ts
+++ app/real-estate-agency/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import {  HomeComponent} from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponent
+    
   ],
   imports: [
     BrowserModule,
