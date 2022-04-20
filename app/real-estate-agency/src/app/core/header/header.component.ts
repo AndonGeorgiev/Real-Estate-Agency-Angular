471f606cde98589d32a4fd47b2cba3adbc537787
AndonGeorgiev <andon2110@gmail.com>
2022-04-20 19:18:56 +0300

    change testing boolean

--- app/real-estate-agency/src/app/core/header/header.component.ts
+++ app/real-estate-agency/src/app/core/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  isUser : boolean = true;
+  isUser : boolean = false;
   isBroker : boolean = true;
   constructor() { }
 
