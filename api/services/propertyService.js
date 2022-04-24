73e9ba6cf98774b53814a3fa4eeda755298a8f6f
AndonGeorgiev <andon2110@gmail.com>
2022-04-24 20:00:20 +0300

    rename edit function

--- api/services/propertyService.js
+++ api/services/propertyService.js
@@ -1,10 +1,11 @@
 const Property = require('./../models/propertyModel.js');
 
 exports.getAll = () => Property.find({});
+
 exports.getOne = async(_id) => {
     let property = await Property.findById(_id);
     const update = { views: +property.views + 1 }
-    await Property.findByIdAndUpdate(_id, update);
+    await Property.edit(_id, update);
     property = await Property.findById(_id);
     return property;
 };
@@ -13,10 +14,11 @@ exports.create = async(propertyInfo) => {
     let newProperty = await Property.create(propertyInfo);
 
     return newProperty;
-}
+};
 
 exports.deleteProperty = async(id) => {
     console.log(2);
     return await Property.findByIdAndDelete(id);
-}
-exports.findByIdAndUpdate = (id, update) => Property.findByIdAndUpdate(id, update);
+};
+
+exports.edit = (id, update) => Property.findByIdAndUpdate(id, update);
